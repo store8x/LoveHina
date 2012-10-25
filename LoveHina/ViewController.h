e11586075964baf640d2680e378c1afcb5552e2b
Sơn Phạm <sonpham@Son-Pham.local>
2012-10-25 10:15:28 +0700

    Initial Commit

---
+++ LoveHina/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LoveHina
+//
+//  Created by Sơn Phạm  on 10/25/12.
+//  Copyright (c) 2012 Sơn Phạm . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ LoveHina/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LoveHina
+//
+//  Created by Sơn Phạm  on 10/25/12.
+//  Copyright (c) 2012 Sơn Phạm . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
